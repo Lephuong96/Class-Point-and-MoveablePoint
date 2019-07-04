6f244011def3abcea2c5dcf4a7c48120735d3065
Lephuong96 <bigankroi@gmail.com>
2019-07-04 21:56:24 +0700

    first commit

---
+++ MoveablePoint.php
@@ -0,0 +1,74 @@
+<?php
+
+
+class MoveablePoint extends Point
+{
+    public $xSpeed;
+    public $ySpeed;
+
+    public function __construct($x, $y, $xSpeed, $ySpeed)
+    {
+        parent::__construct($x, $y);
+        $this->xSpeed = $xSpeed;
+        $this->ySpeed = $ySpeed;
+    }
+
+    /**
+     * @param mixed $xSpeed
+     */
+    public function setXSpeed($xSpeed)
+    {
+        $this->xSpeed = $xSpeed;
+    }
+
+    /**
+     * @param mixed $ySpeed
+     */
+    public function setYSpeed($ySpeed)
+    {
+        $this->ySpeed = $ySpeed;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getXSpeed()
+    {
+        return $this->xSpeed;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYSpeed()
+    {
+        return $this->ySpeed;
+    }
+
+    public function setSpeed($xSpeed, $ySpeed)
+    {
+        $this->xSpeed = $xSpeed;
+        $this->ySpeed = $ySpeed;
+    }
+    public function getSpeed()
+    {
+        $array = ["SpeedX" => $this->xSpeed, "SpeedY" => $this->ySpeed];
+        return $array;
+    }
+    public function toString()
+    {
+        foreach ($this->getXY() as $index => $value) {
+            echo $index, ": ", $value, " ";
+        }
+        echo "<br>";
+        foreach ($this->getSpeed() as $stt => $item) {
+            echo $stt, ":", $item, " ";
+        }
+    }
+
+    public function move(){
+        $this->x += $this->xSpeed;
+        $this->y += $this->ySpeed;
+        return [$this->x,$this->y];
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "Point.php";
+include_once "MoveablePoint.php";
+$moveablePoint = new MoveablePoint(3, 4, 5, 10);
+$moveablePoint->toString();
